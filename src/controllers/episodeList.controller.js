ae7499831865d33207f96384ffdba224f0ee2ae9
iam-SayanDas <sayandas.dev@gmail.com>
2024-08-31 13:14:02 +0530

    updated id

--- src/controllers/episodeList.controller.js
+++ src/controllers/episodeList.controller.js
@@ -2,8 +2,10 @@ import  extractEpisodesList  from "../extractors/episodeList.extractor.js";
 
 export const getEpisodes = async (req, res) => {
   const id = req.params.id;
+  console.log(id);
   try {
     const data = await extractEpisodesList(encodeURIComponent(id));
+    console.log(data);
     res.json({ success: true, results: data });
   } catch (e) {
     console.error(e);
