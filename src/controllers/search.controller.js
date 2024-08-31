b08620a87bb8fe624ff157a211390e97c79dc279
iam-SayanDas <sayandas.dev@gmail.com>
2024-08-31 15:50:47 +0530

    Update search.controller.js

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -6,15 +6,15 @@ export const search = async (req, res) => {
   try {
     const keyword = req.query.keyword;
     let page = parseInt(req.query.page) || 1;
-    // const totalPages = await countPages(
-    //   `https://${v1_base_url}/search?keyword=${keyword}`
-    // );
-    // page = Math.min(page, totalPages);
-    // if (page !== parseInt(req.query.page)) {
-    //   return res.redirect(
-    //     `${req.originalUrl.split("?")[0]}?keyword=${keyword}&page=${page}`
-    //   );
-    // }
+    const totalPages = await countPages(
+      `https://${v1_base_url}/search?keyword=${keyword}`
+    );
+    page = Math.min(page, totalPages);
+    if (page !== parseInt(req.query.page)) {
+      return res.redirect(
+        `${req.originalUrl.split("?")[0]}?keyword=${keyword}&page=${page}`
+      );
+    }
     const data = await extractSearchResults(encodeURIComponent(keyword), page);
     res.json({ success: true, results: data });
   } catch (e) {
