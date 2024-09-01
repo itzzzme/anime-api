5597489a21a04010945d30562e26ca932d4e8291
iam-SayanDas <sayandas.dev@gmail.com>
2024-09-01 14:40:51 +0530

    cleaning code

--- src/extractors/animeInfo.extractor.js
+++ src/extractors/animeInfo.extractor.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import formatTitle from "../helper/formatTitle.helper.js";
 import { v1_base_url } from "../utils/base_v1.js";
+import extractRecommendedData from "./recommend.extractor.js";
+import extractRelatedData from "./related.extractor.js";
 
 const baseUrl = v1_base_url;
 
@@ -9,6 +11,7 @@ async function extractAnimeInfo(id) {
   try {
     const resp = await axios.get(`https://${baseUrl}/${id}`);
     const $ = cheerio.load(resp.data);
+
     const title = $(
       "#ani_detail > .ani_detail-stage > .container > .anis-content > .anisc-detail > .film-name"
     ).text();
@@ -23,6 +26,7 @@ async function extractAnimeInfo(id) {
     );
     const data_id = id.split("-").pop();
     const animeInfo = {};
+
     element.each((index, element) => {
       const key = $(element).find(".item-head").text().trim().replace(":", "");
       const value = $(element).find(".name").text().trim();
@@ -35,6 +39,7 @@ async function extractAnimeInfo(id) {
         animeInfo[key] = value;
       }
     });
+
     const season_id = formatTitle(title, data_id);
     const overview = $(
       "#ani_detail > .ani_detail-stage > .container > .anis-content > .anisc-detail > .film-description > .text"
@@ -43,112 +48,9 @@ async function extractAnimeInfo(id) {
       .trim();
     animeInfo["Overview"] = overview;
 
-    const recommended_data = await Promise.all(
-      $(
-        "#main-content .block_area_category .tab-content .block_area-content .film_list-wrap .flw-item"
-      ).map(async (index, element) => {
-        const id = $(element)
-          .find(".film-detail .film-name a")
-          .attr("href")
-          .split("/")
-          .pop();
-        const data_id = $(element).find(".film-poster a").attr("data-id");
-        const title = $(element)
-          .find(".film-detail .film-name a")
-          .text()
-          .trim();
-        const japanese_title = $(element)
-          .find(".film-detail .film-name a")
-          .attr("data-jname")
-          .trim();
-        const poster = $(element).find(".film-poster img").attr("data-src");
-        const $fdiItems = $(".film-detail .fd-infor .fdi-item", element);
-        const showType = $fdiItems
-          .filter((_, item) => {
-            const text = $(item).text().trim().toLowerCase();
-            return ["tv", "ona", "movie", "ova", "special"].some((type) =>
-              text.includes(type)
-            );
-          })
-          .first();
-        const tvInfo = {
-          showType: showType ? showType.text().trim() : "Unknown",
-          duration: $(".film-detail .fd-infor .fdi-duration", element)
-            .text()
-            .trim(),
-        };
+    const recommended_data = await extractRecommendedData($);
+    const related_data = await extractRelatedData($);
 
-        ["sub", "dub", "eps"].forEach((property) => {
-          const value = $(`.tick .tick-${property}`, element).text().trim();
-          if (value) {
-            tvInfo[property] = value;
-          }
-        });
-        return {
-          data_id,
-          id,
-          title,
-          japanese_title,
-          poster,
-          tvInfo,
-        };
-      })
-    );
-    const related_data = await Promise.all(
-      $(
-        "#main-sidebar .block_area_sidebar .block_area-content .cbox-list .cbox-content .anif-block-ul .ulclear li"
-      ).map(async (index, element) => {
-        const id = $(element)
-          .find(".film-detail .film-name a")
-          .attr("href")
-          .split("/")
-          .pop();
-        const data_id = $(element).find(".film-poster").attr("data-id");
-        const title = $(element)
-          .find(".film-detail .film-name a")
-          .text()
-          .trim();
-        const japanese_title = $(element)
-          .find(".film-detail .film-name a")
-          .attr("data-jname")
-          .trim();
-        const poster = $(element).find(".film-poster img").attr("data-src");
-        const $fdiItems = $(".film-detail>.fd-infor>.tick", element);
-        const showType = $fdiItems
-          .filter((_, item) => {
-            const text = $(item).text().trim().toLowerCase();
-            return ["tv", "ona", "movie", "ova", "special"].some((type) =>
-              text.includes(type)
-            );
-          })
-          .first()
-          .text()
-          .trim()
-          .split(/\s+/)
-          .find((word) =>
-            ["tv", "ona", "movie", "ova", "special"].includes(
-              word.toLowerCase()
-            )
-          );
-        const tvInfo = {
-          showType: showType ? showType : "Unknown",
-        };
-        ["sub", "dub", "eps"].forEach((property) => {
-          const value = $(`.tick .tick-${property}`, element).text().trim();
-          if (value) {
-            tvInfo[property] = value;
-          }
-        });
-        return {
-          data_id,
-          id,
-          title,
-          japanese_title,
-          poster,
-          tvInfo,
-        };
-      })
-    );
     return {
       data_id,
       id: season_id,
---
+++ src/extractors/related.extractor.js
@@ -0,0 +1,62 @@
+export default async function extractRelatedData($) {
+  const relatedElements = $(
+    "#main-sidebar .block_area_sidebar .block_area-content .cbox-list .cbox-content .anif-block-ul .ulclear li"
+  );
+  return await Promise.all(
+    relatedElements
+      .map(async (index, element) => {
+        const id = $(element)
+          .find(".film-detail .film-name a")
+          .attr("href")
+          .split("/")
+          .pop();
+        const data_id = $(element).find(".film-poster").attr("data-id");
+        const title = $(element)
+          .find(".film-detail .film-name a")
+          .text()
+          .trim();
+        const japanese_title = $(element)
+          .find(".film-detail .film-name a")
+          .attr("data-jname")
+          .trim();
+        const poster = $(element).find(".film-poster img").attr("data-src");
+        const $fdiItems = $(".film-detail>.fd-infor>.tick", element);
+        const showType = $fdiItems
+          .filter((_, item) => {
+            const text = $(item).text().trim().toLowerCase();
+            return ["tv", "ona", "movie", "ova", "special"].some((type) =>
+              text.includes(type)
+            );
+          })
+          .first()
+          .text()
+          .trim()
+          .split(/\s+/)
+          .find((word) =>
+            ["tv", "ona", "movie", "ova", "special"].includes(
+              word.toLowerCase()
+            )
+          );
+        const tvInfo = {
+          showType: showType ? showType : "Unknown",
+        };
+
+        ["sub", "dub", "eps"].forEach((property) => {
+          const value = $(`.tick .tick-${property}`, element).text().trim();
+          if (value) {
+            tvInfo[property] = value;
+          }
+        });
+
+        return {
+          data_id,
+          id,
+          title,
+          japanese_title,
+          poster,
+          tvInfo,
+        };
+      })
+      .get()
+  );
+}
