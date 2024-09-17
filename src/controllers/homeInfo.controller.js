e3258b7c20aefeb9d9c2a33945eb60e600acd253
iam-SayanDas <sayandas.dev@gmail.com>
2024-09-17 13:43:19 +0530

    update: added more info to homeinfo

--- src/controllers/homeInfo.controller.js
+++ src/controllers/homeInfo.controller.js
@@ -1,12 +1,53 @@
 import * as spotlightController from "../helper/spotlight.helper.js";
 import * as trendingController from "../helper/trending.helper.js";
+import extractPage from "../helper/extractPages.helper.js";
+import extractTopTen from "../extractors/topten.extractor.js";
+import { routeTypes } from "../routes/category.route.js";
+import extractSchedule from "../extractors/schedule.extractor.js";
+
+const genres = routeTypes
+  .slice(0, 41)
+  .map((genre) => genre.replace("genre/", ""));
+
 export const getHomeInfo = async (req, res) => {
   try {
-    const [spotlights, trending] = await Promise.all([
+    const [
+      spotlights,
+      trending,
+      topTen,
+      schedule,
+      topAiring,
+      mostPopular,
+      mostFavorite,
+      latestCompleted,
+      latestEpisode,
+    ] = await Promise.all([
       spotlightController.getSpotlights(),
       trendingController.getTrending(),
+      extractTopTen(),
+      extractSchedule(new Date().toISOString().split("T")[0]),
+      extractPage(1, "top-airing"),
+      extractPage(1, "most-popular"),
+      extractPage(1, "most-favorite"),
+      extractPage(1, "completed"),
+      extractPage(1, "recently-updated"),
     ]);
-    res.json({ success: true, results: { spotlights, trending } });
+
+    res.json({
+      success: true,
+      results: {
+        spotlights,
+        trending,
+        topTen,
+        today: { schedule },
+        topAiring,
+        mostPopular,
+        mostFavorite,
+        latestCompleted,
+        latestEpisode,
+        genres,
+      },
+    });
   } catch (e) {
     console.error(e);
     res.status(500).json({ success: false, error: "Internal Server Error" });
