8e00543e73de3144667e4efc5fc1b0e9b1822663
iam-SayanDas <sayandas.dev@gmail.com>
2024-09-17 13:13:56 +0530

    update: added more label in topten category

--- src/extractors/topten.extractor.js
+++ src/extractors/topten.extractor.js
@@ -11,29 +11,39 @@ async function extractTopTen() {
     const resp = await axios.get("/home");
     const $ = cheerio.load(resp.data);
 
-    const data = $(
-      "#main-sidebar .block_area-realtime .block_area-content ul:eq(0)>li"
-    )
-      .map((index, element) => {
-        const number = $(".film-number>span", element).text().trim();
-        const title = $(".film-detail>.film-name>a", element).text().trim();
-        const poster = $(".film-poster>img", element).attr("data-src");
-        const japanese_title=$(".film-detail>.film-name>a", element).attr("data-jname").trim();
-        const data_id = $(".film-poster", element).attr("data-id");
-        const id=formatTitle(title, data_id);
-        const tvInfo = ["sub", "dub", "eps"].reduce((info, property) => {
-          const value = $(`.tick .tick-${property}`, element).text().trim();
-          if (value) {
-            info[property] = value;
-          }
-          return info;
-        }, {});
+    const labels = ["today", "week", "month"];
+    const result = {};
 
-        return {id, data_id, number, title,japanese_title, poster, tvInfo };
-      })
-      .get();
+    // Loop through eq(0), eq(1), eq(2) for today, week, and month respectively
+    labels.forEach((label, idx) => {
+      const data = $(
+        `#main-sidebar .block_area-realtime .block_area-content ul:eq(${idx})>li`
+      )
+        .map((index, element) => {
+          const number = $(".film-number>span", element).text().trim();
+          const title = $(".film-detail>.film-name>a", element).text().trim();
+          const poster = $(".film-poster>img", element).attr("data-src");
+          const japanese_title = $(".film-detail>.film-name>a", element)
+            .attr("data-jname")
+            .trim();
+          const data_id = $(".film-poster", element).attr("data-id");
+          const id = formatTitle(title, data_id);
+          const tvInfo = ["sub", "dub", "eps"].reduce((info, property) => {
+            const value = $(`.tick .tick-${property}`, element).text().trim();
+            if (value) {
+              info[property] = value;
+            }
+            return info;
+          }, {});
 
-    return data;
+          return { id, data_id, number, title, japanese_title, poster, tvInfo };
+        })
+        .get();
+
+      result[label] = data;
+    });
+
+    return result;
   } catch (error) {
     console.error("Error fetching data:", error);
     throw error;
