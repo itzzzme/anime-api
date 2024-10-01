77373649ce113681fc170927a580f845f6b9f2aa
iam-SayanDas <sayandas.devs@gmail.com>
2024-10-02 01:17:27 +0530

    update: changed strategy to store cache data

--- src/helper/cache.helper.js
+++ src/helper/cache.helper.js
@@ -4,15 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const CACHE_SERVER_URL = process.env.CACHE_URL;
-const ORIGIN_URL = process.env.ORIGIN_URL;
 
 export const getCachedData = async (key) => {
   try {
-    const response = await axios.get(`${CACHE_SERVER_URL}/${key}`, {
-      headers: {
-        Origin: ORIGIN_URL,
-      },
-    });
+    const response = await axios.get(`${CACHE_SERVER_URL}/${key}`);
     return response.data;
   } catch (error) {
     if (error.response && error.response.status === 404) {
@@ -24,15 +19,7 @@ export const getCachedData = async (key) => {
 
 export const setCachedData = async (key, value) => {
   try {
-    await axios.post(
-      CACHE_SERVER_URL,
-      { key, value },
-      {
-        headers: {
-          Origin: ORIGIN_URL,
-        },
-      }
-    );
+    await axios.post(CACHE_SERVER_URL, { key, value });
   } catch (error) {
     console.error("Error setting cache data:", error);
     throw error;
