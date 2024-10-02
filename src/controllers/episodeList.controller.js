a7a3e41e63eb4609b7afcebe2234c1ea18519697
iam-SayanDas <sayandas.devs@gmail.com>
2024-10-02 14:04:31 +0530

    update: implemented caching in episodes route

--- src/controllers/episodeList.controller.js
+++ src/controllers/episodeList.controller.js
@@ -1,12 +1,22 @@
 import extractEpisodesList from "../extractors/episodeList.extractor.js";
+import { getCachedData, setCachedData } from "../helper/cache.helper.js";
 
 export const getEpisodes = async (req, res) => {
   const id = req.params.id;
+  const cacheKey = `episodes_${id}`; 
+
   try {
+    const cachedResponse = await getCachedData(cacheKey);
+    if (cachedResponse) {
+      return res.json({ success: true, results: cachedResponse });
+    }
+
     const data = await extractEpisodesList(encodeURIComponent(id));
-    res.json({ success: true, results: data });
+    await setCachedData(cacheKey, data);
+
+    return res.json({ success: true, results: data });
   } catch (e) {
-    console.error(e);
+    console.error("Error fetching episodes:", e);
     res.status(500).json({ success: false, error: "Internal Server Error" });
   }
 };
