d654a4ffa8f0df34ade3252d3d2997c989e813db
iam-SayanDas <sayandas.devs@gmail.com>
2024-10-02 20:49:17 +0530

    update: simplifying structure of category.controller

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,5 +1,32 @@
-import * as handleRequestHelper from "../helper/handleRequest.helper.js";
+import { extractor } from "../extractors/category.extractor.js";
+import { getCachedData, setCachedData } from "../helper/cache.helper.js";
 
 export const getCategory = async (req, res, routeType) => {
-  await handleRequestHelper.handleRequest(req, res, routeType);
+  const requestedPage = parseInt(req.query.page) || 1;
+  const cacheKey = `${routeType.replace(/\//g, '_')}_page_${requestedPage}`;
+  
+  try {
+    const cachedResponse = await getCachedData(cacheKey);
+    if (cachedResponse) {
+      return res.json(cachedResponse);
+    }
+    
+    const { data, totalPages } = await extractor(routeType, requestedPage);
+
+    const pageToRedirect = Math.min(requestedPage, totalPages);
+    if (pageToRedirect !== requestedPage) {
+      return res.redirect(
+        `${req.originalUrl.split("?")[0]}?page=${pageToRedirect}`
+      );
+    }
+
+    const responseData = { success: true, results: { totalPages, data } };
+
+    await setCachedData(cacheKey, responseData);
+
+    res.json(responseData);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
