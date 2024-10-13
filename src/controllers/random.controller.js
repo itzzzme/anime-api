675ca9513d9441cbb33fa78c5de8a649253e7db9
iam-SayanDas <sayandas.devs@gmail.com>
2024-10-13 14:14:50 +0530

    update: updated the structure of random route

--- src/controllers/random.controller.js
+++ src/controllers/random.controller.js
@@ -1,11 +1,17 @@
 import extractRandom from "../extractors/random.extractor.js";
 
 export const getRandom = async (req, res) => {
-    try {
-        const random = await extractRandom();
-        res.json(random);
-    } catch (error) {
-        console.error("Error getting random anime:", error.message);
-        res.status(500).json({ error: "An error occurred while getting random anime" });
-    }
-}
+  try {
+    const random = await extractRandom();
+    res.json({
+      success: true,
+      results: random,
+    });
+  } catch (error) {
+    console.error("Error getting random anime:", error.message);
+    res.status(500).json({
+      success: false,
+      error: "An error occurred while getting random anime",
+    });
+  }
+};
