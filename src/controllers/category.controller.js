1d5ae45c8322d2e79ed2ec9ea18ff01e9f42fa9f
iam-SayanDas <sayandas.devs@gmail.com>
2024-10-20 01:34:40 +0530

    update: added error handling on page overflow in category route

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -11,11 +11,11 @@ export const getCategory = async (c, routeType) => {
     const cachedResponse = await getCachedData(cacheKey);
     if (cachedResponse) return cachedResponse;
     const { data, totalPages } = await extractor(routeType, requestedPage);
-    // if (requestedPage > totalPages) {
-    //   const error = new Error("Requested page exceeds total available pages.");
-    //   error.status = 404;
-    //   throw error;
-    // }
+    if (requestedPage > totalPages) {
+      const error = new Error("Requested page exceeds total available pages.");
+      error.status = 404;
+      throw error;
+    }
     const responseData = { totalPages: totalPages, data: data };
     setCachedData(cacheKey, responseData).catch((err) => {
       console.error("Failed to set cache:", err);
