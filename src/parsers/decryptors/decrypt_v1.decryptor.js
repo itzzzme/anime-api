4b5847427980d031ca2d10b7b8e74d53f69daca0
itzzzme <sayandas.devs@gmail.com>
2025-08-20 15:50:26 +0530

    Fixed broken streaming link

--- src/parsers/decryptors/decrypt_v1.decryptor.js
+++ src/parsers/decryptors/decrypt_v1.decryptor.js
@@ -3,6 +3,7 @@ import CryptoJS from "crypto-js";
 import { v1_base_url } from "../../utils/base_v1.js";
 import { fallback_1, fallback_2 } from "../../utils/fallback.js";
 import extractToken from "../../helper/token.helper.js";
+import { v4_base_url } from "../../utils/base_v4.js";
 
 export async function decryptSources_v1(epID, id, name, type) {
   try {
@@ -10,8 +11,10 @@ export async function decryptSources_v1(epID, id, name, type) {
     //   axios.get(`https://${v1_base_url}/ajax/v2/episode/sources?id=${id}`),
     //   axios.get("https://raw.githubusercontent.com/itzzzme/megacloud-keys/refs/heads/main/key.txt"),
     // ]);
+    console.log(`https://${v4_base_url}/ajax/episode/sources?id=${id}`);
     const { data: sourcesData } = await axios.get(
-      `https://${v1_base_url}/ajax/v2/episode/sources?id=${id}`,
+      // `https://${v1_base_url}/ajax/v2/episode/sources?id=${id}`,
+      `https://${v4_base_url}/ajax/episode/sources?id=${id}`,
     );
     const ajaxLink = sourcesData?.link;
     if (!ajaxLink) throw new Error("Missing link in sourcesData");
@@ -25,11 +28,11 @@ export async function decryptSources_v1(epID, id, name, type) {
       throw new Error("Could not extract base URL from ajaxLink");
     const baseUrl = baseUrlMatch[1];
     const iframeURL = `${baseUrl}/${sourceId}?k=1&autoPlay=0&oa=0&asi=1`;
-
+    console.log(`${baseUrl}/getSources?id=${sourceId}`);
     const { data: rawSourceData } = await axios.get(
-      `https://decrypt.zenime.site/extract?embed_url=${iframeURL}`,
+      `${baseUrl}/getSources?id=${sourceId}`,
     );
-    const decryptedSources = rawSourceData.data;
+    const decryptedSources = rawSourceData;
     return {
       id,
       type,
