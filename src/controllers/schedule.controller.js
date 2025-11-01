d44ab6b8530a0d5f589e6629f2ce4f9f94994a4b
Shahid Alam <shahidalam0096@gmail.com>
2025-11-01 12:54:00 +0530

    Update schedule.controller.js

--- src/controllers/schedule.controller.js
+++ src/controllers/schedule.controller.js
@@ -2,8 +2,9 @@ import extractSchedule from "../extractors/schedule.extractor.js";
 
 export const getSchedule = async (req) => {
   const date = req.query.date;
+  const tzOffset = req.query.tzOffset || -330;
   try {
-    const data = await extractSchedule(encodeURIComponent(date));
+    const data = await extractSchedule(date, tzOffset);
     return data;
   } catch (e) {
     console.error(e);
